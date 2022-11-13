6ecf356987d23d5235c1e727da35acff1a41f536
Zahra1205 <78679374+Zahra1205@users.noreply.github.com>
2022-11-13 14:36:41 +0530

    Add files via upload

--- fruit_basket.js
+++ fruit_basket.js
@@ -1,9 +1,26 @@
+var img="";
+var status="";
 function preload(){
     img = loadImage("fruit_basket.jpg");
     }
     function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector= ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function modelLoaded(){
+console.log("cocossd Model Loaded");
+status= true;
+objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+}
     }
     function draw(){
     image(img, 0, 0, 640, 420);
